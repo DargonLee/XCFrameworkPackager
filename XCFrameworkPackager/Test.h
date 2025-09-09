e760c45e4e9619d856285ecb21a442c527c14a18
hailong.li <2461414445@qq.com>
2025-09-09 20:23:59 +0800

    add oc file

---
+++ XCFrameworkPackager/Test.h
@@ -0,0 +1,16 @@
+//
+//  Test.h
+//  XCFrameworkPackager
+//
+//  Created by Harlan on 2025/8/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XCFrameworkPackager/Test.m
@@ -0,0 +1,17 @@
+//
+//  Test.m
+//  XCFrameworkPackager
+//
+//  Created by Harlan on 2025/8/30.
+//
+
+#import "Test.h"
+
+@implementation Test
+- (void)test
+{
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    
+    // [NSOperationQueue addOperation:]
+}
+@end
